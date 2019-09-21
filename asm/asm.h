ea54138023cb55612ad2f9ca439a29fc551db1fa
Black To The Bone <blacktothebone@AirMac.local>
2019-09-21 20:02:24 +0300

    ASM: forgot to push modified files concerning validation of input

--- asm/asm.h
+++ asm/asm.h
@@ -2,5 +2,9 @@
 # define ASM_H
 
 # include "op.h"
+# include "libft/libft.h"
+# include <stdio.h> //DELETE THEN
+
+void	check_input(int argc, char **argv);
 
 #endif
--- asm/main.c
+++ asm/main.c
@@ -1,6 +1,8 @@
 #include "asm.h"
 
-int		main(void)
+int		main(int argc, char **argv)
 {
+	check_input(argc, argv);
+	system("leaks -q asm");
 	return (0);
 }
