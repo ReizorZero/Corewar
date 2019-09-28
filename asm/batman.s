cacb572e490df278fa30dfc99d4eaef6315cc379
gottagoblast <double0jeopardy@gmail.com>
2019-09-28 18:36:50 +0300

    blueprints on command lines parsing (divide word by word)

--- asm/batman.s
+++ asm/batman.s
@@ -1,10 +1,7 @@
 .name     "Batman" #kamment
 .comment   "This city needs me" #kamment
- 
-#live %0
-                    #ische kamment    
 loop: 
-    sti             r1,           %:live,             %1
+    sti r1,%:live,%1
 
 live:
     live %0
