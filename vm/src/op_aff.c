aba46aa9bee7dee052c95cfa62669a02bd03890a
mishvets <19mch97@gmail.com>
2019-10-08 19:59:09 +0300

    norminette op_aff.c

--- vm/src/op_aff.c
+++ vm/src/op_aff.c
@@ -12,16 +12,16 @@
 
 #include "../inc/corewar_vm.h"
 
-void op_aff(t_general *data, t_carriage *carriage)
+void	op_aff(t_general *data, t_carriage *carriage)
 {
 	char val;
 
 	carriage->position_tmp = (carriage->position + 2) % MEM_SIZE;
-	if (arg_read(data, carriage, data->mem_f[(carriage->position + 1) % MEM_SIZE]))
+	if (arg_read(data, carriage, data->mem_f[(carriage->position + 1)
+		% MEM_SIZE]))
 	{
 		val = (char)get_val32bit(carriage->arg[0]);
 		ft_printf("Aff: %c\n", val);
-//		carriage->carry = 0;
 	}
 	show_pc_movement(*data, *carriage);
 	carriage->position = carriage->position_tmp;
