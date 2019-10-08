f73f7977b4190b7674020651a1cb9bfc6ffaf5af
mishvets <19mch97@gmail.com>
2019-10-08 19:51:39 +0300

    norminette op_add.c op_sub.c

--- vm/src/op_add.c
+++ vm/src/op_add.c
@@ -12,22 +12,24 @@
 
 #include "../inc/corewar_vm.h"
 
-void op_add(t_general *data, t_carriage *carriage)
+void	op_add(t_general *data, t_carriage *carriage)
 {
 	uint32_t val1;
 	uint32_t val2;
 
 	carriage->position_tmp = (carriage->position + 2) % MEM_SIZE;
-	if (arg_read(data, carriage, data->mem_f[(carriage->position + 1) % MEM_SIZE]))
+	if (arg_read(data, carriage, data->mem_f[(carriage->position + 1)
+		% MEM_SIZE]))
 	{
 		val1 = get_val32bit(carriage->arg[0]);
 		val2 = get_val32bit(carriage->arg[1]);
 		*(uint32_t*)carriage->arg[2].mem = reverse_32bits(val1 + val2);
 		carriage->carry = !(*(uint32_t*)carriage->arg[2].mem);
-		if (data->verb_nbr & 4) //verb_nbr 4
+		if (data->verb_nbr & 4)
 		{
-			ft_printf("P %4d | add r%d r%d r%d\n", carriage->nbr, get_num_reg(carriage, 0),
-					  get_num_reg(carriage, 1), get_num_reg(carriage, 2));
+			ft_printf("P %4d | add r%d r%d r%d\n", carriage->nbr,
+				get_num_reg(carriage, 0), get_num_reg(carriage, 1),
+				get_num_reg(carriage, 2));
 		}
 	}
 	show_pc_movement(*data, *carriage);
