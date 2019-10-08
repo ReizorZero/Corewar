60bd38affad11a10d0aa1bd16e28832c5d3ae66b
gottagoblast <double0jeopardy@gmail.com>
2019-10-08 17:16:08 +0300

    ASM: (almost) final version (works, but dirty as hell and strongly needs refactoring)

--- asm/batman.s
+++ asm/batman.s
@@ -4,11 +4,14 @@ loop:#:
     sti r1,%:live,%1 #ghjkl <-- ain't detects error here
 sti r1 , %:live ,    %1 #ghjkl <-- detects error here
 loop:sti r1,%:live,%1
-#blep:sti r1,%:live %1
 loop: sti r1 , %:live ,    %1
 loop: sti r1,r2,    r3
-loop: sti r1,r2   ,    r3
+loop: sti r1,r3   ,    r3
 live:
+death:
+chetotam:
     live %0
     ld %0, r2
     zjmp %:loop
+
+empty:
--- asm/check_command_line.c
+++ asm/check_command_line.c
@@ -300,7 +300,7 @@ int		check_if_words_correct(t_asm *the_asm, t_line **line)
 	// sep_chars = 0;
 	i = 0;
 	s = (*line)->str;
-	printf("START OF CHECKING...\n");
+	//printf("START OF CHECKING...\n");
 	if (first_is_label)
 	{
 		while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
@@ -429,7 +429,34 @@ int		check_if_words_correct(t_asm *the_asm, t_line **line)
 		}
 		// printf("%s\n", &s[i]);
 	}
-	return (1);
+	return (first_is_label);
+}
+
+void	add_cmnd_line(t_asm *the_asm)
+{
+	if (!the_asm->e_c_l)
+		the_asm->e_c_l = new_exec_code_line(the_asm->last_cmnd_id);
+	else
+	{
+		the_asm->e_c_l->next = new_exec_code_line(the_asm->last_cmnd_id);
+		the_asm->e_c_l = the_asm->e_c_l->next;
+	}
+	if (!the_asm->e_c_l_top)
+		the_asm->e_c_l_top = the_asm->e_c_l;
+	the_asm->last_cmnd_id++;
+}
+
+void	add_label(t_asm *the_asm)
+{
+	if (!the_asm->labels)
+		the_asm->labels = new_label(the_asm->line_words[0]);
+	else
+	{
+		the_asm->labels->next = new_label(the_asm->line_words[0]);
+		the_asm->labels = the_asm->labels->next;
+	}
+	if (!the_asm->labels_top)
+		the_asm->labels_top = the_asm->labels;
 }
 
 int		is_empty_or_comment(t_line **line)
@@ -450,11 +477,125 @@ int		is_empty_or_comment(t_line **line)
 	return (1);
 }
 
+void	fill_cmnd_line_info(t_asm *the_asm, int index)
+{
+	int i;
+
+	i = 0;
+	while (i < COMMANDS_N)
+	{
+		if (!ft_strcmp(the_asm->line_words[index],
+		commands[i].name))
+		{
+			the_asm->e_c_l->cmnd_code = commands[i].id;
+			printf("COMMAND: %s\n", commands[i].name);
+			break ;
+		}
+		i++;
+	}
+	//ARG_1
+	if (the_asm->line_words[index + 1][0] == 'r')
+	{
+		the_asm->e_c_l->arg_1_code = REG_CODE;
+		the_asm->e_c_l->arg_1_value = ft_atoi(&the_asm->line_words[index + 1][1]);
+		printf("ARG_1: %i\n", the_asm->e_c_l->arg_1_value);
+	}
+	else if (the_asm->line_words[index + 1][0] == '%')
+	{
+		the_asm->e_c_l->arg_1_code = DIR_CODE;
+		//IF IT IS NOT LABEL
+		the_asm->e_c_l->arg_1_value = ft_atoi(&the_asm->line_words[index + 1][1]);
+		//IF IT IS A LABEL, FIND THE CORRESPONDING LABEL IN LABELS LIST
+		//AND WRITE ITS BYTE_AT VALUE
+		printf("ARG_1: %i\n", the_asm->e_c_l->arg_1_value);
+	}
+	else
+	{
+		the_asm->e_c_l->arg_1_code = IND_CODE;
+		//IF IT IS NOT LABEL
+		the_asm->e_c_l->arg_1_value = ft_atoi(&the_asm->line_words[index + 1][0]);
+		//IF IT IS A LABEL, FIND THE CORRESPONDING LABEL IN LABELS LIST
+		//AND WRITE ITS BYTE_AT VALUE
+		printf("ARG_1: %i\n", the_asm->e_c_l->arg_1_value);
+	}
+	printf("ARG_1 TYPE: %i\n", the_asm->e_c_l->arg_1_code);
+	//CHECK IF THE TYPES OF ARGS ARE CORRESPONDING WITH THE COMMAND
+	int k;
+	int found_type;
+	k = 0;
+	found_type = 0;
+	while (k < 3)
+	{
+		if (the_asm->e_c_l->arg_1_code == commands[the_asm->e_c_l->cmnd_code - 1].arg_1_types[k])
+		{
+			printf("type = %i\n", commands[the_asm->e_c_l->cmnd_code - 1].arg_1_types[k]);
+			found_type = 1;
+			break ;
+		}
+		printf("type @ %i\n", commands[the_asm->e_c_l->cmnd_code - 1].arg_1_types[k]);
+		k++;
+	}
+	if (!found_type)
+	{
+		printf("Wrong arg_1 type.\n");
+		exit (0);
+	}
+	//DO THE SAME FOR THE REST 2 ARGS: ARG_2, ARG_3
+	the_asm->e_c_l->cmnd_size = 1 + commands[the_asm->e_c_l->cmnd_code - 1].has_arg_types_code +
+	the_asm->e_c_l->arg_1_size + the_asm->e_c_l->arg_2_size + the_asm->e_c_l->arg_3_size;
+}
+
+int		count_label_byte_at(t_asm *the_asm)
+{
+	int i;
+	int id;
+	int byte_at;
+	t_exec_code_line *cmnd_line;
+
+	i = 0;
+	id = the_asm->last_cmnd_id;
+	byte_at = 0;
+	cmnd_line = the_asm->e_c_l_top;
+	while (i < id && cmnd_line)
+	{
+		byte_at += cmnd_line->cmnd_size;
+		cmnd_line = cmnd_line->next;//AVOID SEGV HERE
+		i++;
+	}
+	return (byte_at);
+}
+
 void	check_command_line(t_asm *the_asm, t_line **line)
 {
+	int first_is_label;
+	int index;
+
 	if (!is_empty_or_comment(line))
 	{
 		write_words(the_asm, line);
-		check_if_words_correct(the_asm, line);
+		first_is_label = check_if_words_correct(the_asm, line);
+		if (first_is_label)
+		{
+
+			//IF WE HAVE LABEL
+			//CREATE NEW LABEL
+			add_label(the_asm);
+			//COUNT SIZE OF COMMANDS BEFORE
+			//count_label_byte_at(the_asm);//add here or manually:
+			//the_asm->labels->byte_at = count_label_byte_at(the_asm);
+			if (the_asm->line_words[1][0] == '\0')
+				index = -1;
+			else
+				index = 1;
+		}
+		else
+			index = 0;
+		if (index > -1)
+		{
+			add_cmnd_line(the_asm);
+			fill_cmnd_line_info(the_asm, index);
+		}
+		//ADD COMMAND LINE
+		//WRITE IT TO STRUCTURE
 	}
 }
