07f3d03366419999ca2e74bd229616f8bfbbb07c
mishvets <19mch97@gmail.com>
2019-10-08 20:30:48 +0300

    norminette op_ld.c op_lld.c

--- vm/src/op_ld.c
+++ vm/src/op_ld.c
@@ -12,24 +12,25 @@
 
 #include "../inc/corewar_vm.h"
 
-void op_ld(t_general *data, t_carriage *carriage)
+void	op_ld(t_general *data, t_carriage *carriage)
 {
 	carriage->position_tmp = (carriage->position + 2) % MEM_SIZE;
-	if (arg_read(data, carriage, data->mem_f[(carriage->position + 1) % MEM_SIZE]))
+	if (arg_read(data, carriage, data->mem_f[(carriage->position + 1)
+		% MEM_SIZE]))
 	{
 		memory_cpy(&carriage->arg[1], carriage->arg[0]);
 		carriage->carry = !(get_val32bit(carriage->arg[0]));
-		if (data->verb_nbr & 4) //verb_nbr 4
+		if (data->verb_nbr & 4)
 		{
 			if (carriage->arg[1].type == T_IND)
 			{
 				ft_printf("P %4d | ld %d r%d\n", carriage->nbr,
-						  carriage->arg[0].IND_pntr, get_num_reg(carriage, 1));
+					carriage->arg[0].IND_pntr, get_num_reg(carriage, 1));
 			}
 			else
 			{
 				ft_printf("P %4d | ld %d r%d\n", carriage->nbr,
-						get_val32bit(carriage->arg[0]), get_num_reg(carriage, 1));
+					get_val32bit(carriage->arg[0]), get_num_reg(carriage, 1));
 			}
 		}
 	}
