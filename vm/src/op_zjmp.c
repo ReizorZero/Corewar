af07f88c1a807c4f6be1c7852bfcb100a11b74e5
mishvets <19mch97@gmail.com>
2019-10-08 22:05:12 +0300

    norminette op_live.c op_zjmp.c

--- vm/src/op_zjmp.c
+++ vm/src/op_zjmp.c
@@ -11,28 +11,24 @@
 /* ************************************************************************** */
 
 #include "../inc/corewar_vm.h"
-//#include <stdio.h>
 
-void op_zjmp(t_general *data, t_carriage *carriage)
+void	op_zjmp(t_general *data, t_carriage *carriage)
 {
 	int16_t	dist;
 
 	carriage->position_tmp = (carriage->position + 1) % MEM_SIZE;
 	arg_read(data, carriage, 0b10000000);
 	dist = (int16_t)get_val16bit(carriage->arg[0]);
-	if (data->verb_nbr & 4) //verb_nbr 4
+	if (data->verb_nbr & 4)
 	{
-			ft_printf("P %4d | zjmp %d %s\n", carriage->nbr, dist,
-				(carriage->carry ? "OK" : "FAILED"));
+		ft_printf("P %4d | zjmp %d %s\n", carriage->nbr, dist,
+			(carriage->carry ? "OK" : "FAILED"));
 	}
 	if (carriage->carry)
-	{
-		carriage->position = (carriage->position + dist  % IDX_MOD) % MEM_SIZE;
-	}
+		carriage->position = (carriage->position + dist % IDX_MOD) % MEM_SIZE;
 	else
 	{
 		show_pc_movement(*data, *carriage);
 		carriage->position = carriage->position_tmp;
 	}
-//	carriage->carry = 0;
 }
