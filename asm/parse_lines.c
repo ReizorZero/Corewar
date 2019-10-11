560325da5dc8333e4c7cbfe51958d97727723a78
gottagoblast <double0jeopardy@gmail.com>
2019-10-11 18:36:59 +0300

    ASM: writing exec_code_size to the_asm

--- asm/parse_lines.c
+++ asm/parse_lines.c
@@ -1,5 +1,19 @@
 #include "asm.h"
 
+void	count_exec_size(t_asm *the_asm)
+{
+	t_exec_code_line *ecl;
+
+	ecl = the_asm->e_c_l_top;
+	while (ecl)
+	{
+		printf("@ cmnd_line_size: %i\n", ecl->cmnd_line_size);
+		the_asm->exec_code_size += ecl->cmnd_line_size;
+		ecl = ecl->next;
+	}
+	printf("exec_code_size = %i\n", the_asm->exec_code_size);
+}
+
 void	parse_lines(t_asm *the_asm)
 {
 	t_line *lines;
@@ -17,4 +31,5 @@ void	parse_lines(t_asm *the_asm)
 		lines = lines->next;
 	}
 	fill_labels_args(the_asm);
+	count_exec_size(the_asm);
 }
