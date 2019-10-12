ae85443fe61c304acabc7807f180358b1db27f9d
Black To The Bone <blacktothebone@AirMac.local>
2019-10-13 02:54:53 +0300

    ASM: fixin' dat stuff

--- asm/check_if_words_correct.c
+++ asm/check_if_words_correct.c
@@ -38,39 +38,35 @@ void	count_words_cmnd_line(t_asm *the_asm)
 	the_asm->e_c_l->words = words;
 }
 
-void	check_separators(t_asm *the_asm, t_line **line)
+int		go_past_label(char *s, int i)
 {
-	char	*s;
-	int		i;
-	int		words;
-
-	i = 0;
-	s = (*line)->str;
-	words = the_asm->e_c_l->words;
-	
-	if (the_asm->e_c_l->first_is_label)
-	{
-		while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
+	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
 			i++;
-		while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' && s[i] != LABEL_CHAR)
+	while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' && s[i] != LABEL_CHAR)
 			i++;
-		i++;//WENT PAST LABEL
-	}
+	i++;//WENT PAST LABEL
+	return (i);
+}
 
+int		go_past_command(t_asm *the_asm, char *s, int i)
+{
 	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
 		i++;
 	while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' && s[i] != SEPARATOR_CHAR)
 		i++;//WENT PAST COMMAND
 	if (s[i] == SEPARATOR_CHAR)
 		ERROR(SEPARATOR_AFTER_CMND, the_asm->curr_line_n);
+	return (i);
+}
 
+int		go_past_arg_1(t_asm *the_asm, char *s, int i, int words)
+{
 	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
 		i++;
 	while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' && s[i] != SEPARATOR_CHAR)
 		i++;//WENT PAST FIRST ARG
 	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
 		i++;
-
 	if (s[i] == SEPARATOR_CHAR &&
 	(words == 2 + the_asm->e_c_l->first_is_label))
 		ERROR(WRONG_N_SEPARATORS, the_asm->curr_line_n);
@@ -79,7 +75,11 @@ void	check_separators(t_asm *the_asm, t_line **line)
 		ERROR(WRONG_N_SEPARATORS, the_asm->curr_line_n);
 	if (s[i] == SEPARATOR_CHAR)
 		i++;//','
+	return (i);
+}
 
+int		go_past_arg_2(t_asm *the_asm, char *s, int i, int words)
+{
 	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
 		i++;
 	while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' && s[i] != SEPARATOR_CHAR)
@@ -95,7 +95,11 @@ void	check_separators(t_asm *the_asm, t_line **line)
 		ERROR(WRONG_N_SEPARATORS, the_asm->curr_line_n);
 	if (s[i] == SEPARATOR_CHAR)
 		i++;//','
+	return (i);
+}
 
+int		go_past_arg_3(t_asm *the_asm, char *s, int i, int words)
+{
 	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t'))
 		i++;
 	while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' && s[i] != SEPARATOR_CHAR)
@@ -105,6 +109,29 @@ void	check_separators(t_asm *the_asm, t_line **line)
 	if (s[i] == SEPARATOR_CHAR &&
 	(words == 4 + the_asm->e_c_l->first_is_label))
 		ERROR(WRONG_N_SEPARATORS, the_asm->curr_line_n);
+	return (i);
+}
+
+void	check_separators(t_asm *the_asm, t_line **line)
+{
+	char	*s;
+	int		i;
+	int		words;
+
+	i = 0;
+	s = (*line)->str;
+	words = the_asm->e_c_l->words;
+	
+	if (the_asm->e_c_l->first_is_label)
+		i = go_past_label(s, i);
+
+	i = go_past_command(the_asm, s, i);
+
+	i = go_past_arg_1(the_asm, s, i, words);
+
+	i = go_past_arg_2(the_asm, s, i, words);
+
+	i = go_past_arg_3(the_asm, s, i, words);
 }
 
 int		check_if_words_correct(t_asm *the_asm, t_line **line)
@@ -115,6 +142,7 @@ int		check_if_words_correct(t_asm *the_asm, t_line **line)
 
 	count_words_cmnd_line(the_asm);
 	words = the_asm->e_c_l->words;
+	//SEPARATE
 	if (the_asm->e_c_l->first_is_label)
 	{
 		check_label(the_asm, the_asm->line_words[0]);
@@ -129,6 +157,7 @@ int		check_if_words_correct(t_asm *the_asm, t_line **line)
 		the_asm->last_cmnd_id--;
 		return (1);
 	}
+	//
 	if ((the_asm->e_c_l->first_is_label && words == 2) ||
 	(!the_asm->e_c_l->first_is_label && words == 1))
 		ERROR(NO_ARGS, the_asm->curr_line_n);
@@ -145,6 +174,7 @@ int		check_if_words_correct(t_asm *the_asm, t_line **line)
 	if (args_n != commands[the_asm->e_c_l->cmnd_code - 1].arg_n)
 		ERROR(WRONG_ARGS_N, the_asm->curr_line_n);
 	check_separators(the_asm, line);
+	//SEPARATE
 	the_asm->e_c_l->has_arg_types_code = 
 	commands[the_asm->e_c_l->cmnd_code - 1].has_arg_types_code;
 	the_asm->e_c_l->cmnd_line_size = 1 +
@@ -152,5 +182,6 @@ int		check_if_words_correct(t_asm *the_asm, t_line **line)
 	the_asm->e_c_l->arg_size[0] +
 	the_asm->e_c_l->arg_size[1] +
 	the_asm->e_c_l->arg_size[2];
+	//
 	return (1);
 }
--- asm/write_cmnd_line_words.c
+++ asm/write_cmnd_line_words.c
@@ -1,7 +1,7 @@
 #include "asm.h"
 
 void	write_cmnd_line_words(t_asm *the_asm, t_line **line)
-{
+{//VSE LISHNIYE PEREMENNIYE TIPA MET_SEP SDELAT' POLYAMI STRUKTURY LINE
 	char	*s;
 	int		i;
 	int		start;
@@ -18,17 +18,24 @@ void	write_cmnd_line_words(t_asm *the_asm, t_line **line)
 	{
 		if (w_i == 0)
 		{
+			//SEPARATE
 			while (s[i] != '\0' && (s[i] == ' '|| s[i] == '\t') &&
 			s[i] != COMMENT_CHAR && s[i] != ALT_COMMENT_CHAR)
 				i++;
+			//
 			start = i;
 			len = 0;
+			
+			//SEPARATE
 			while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' &&
 			s[i] != LABEL_CHAR && s[i] != COMMENT_CHAR && s[i] != ALT_COMMENT_CHAR)
 			{
 				len++;
 				i++;
 			}
+			//
+
+			//SEPARATE
 			if (s[i] == LABEL_CHAR)
 			{
 				met_label = 1;
@@ -36,9 +43,11 @@ void	write_cmnd_line_words(t_asm *the_asm, t_line **line)
 				len++;
 				i++;
 			}
+			//
 		}
 		else
 		{
+			//SEPARATE
 			while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t') &&
 			s[i] != COMMENT_CHAR && s[i] != ALT_COMMENT_CHAR && s[i] != SEPARATOR_CHAR)
 				i++;
@@ -48,20 +57,28 @@ void	write_cmnd_line_words(t_asm *the_asm, t_line **line)
 				ERROR(SEPARATOR_AFTER_CMND, the_asm->curr_line_n);
 			if (s[i] == SEPARATOR_CHAR && met_label && w_i == 2)
 				ERROR(SEPARATOR_AFTER_CMND, the_asm->curr_line_n);
+			//
 			met_sep = 0;
 			start = i;
 			len = 0;
+			//SEPARATE
 			while (s[i] != '\0' && s[i] != ' ' && s[i] != '\t' &&
 			s[i] != SEPARATOR_CHAR && s[i] != COMMENT_CHAR && s[i] != ALT_COMMENT_CHAR)
 			{
 				len++;
 				i++;
 			}
+			//
+
+			//SEPARATE
 			if (s[i] == SEPARATOR_CHAR)
 			{
 				met_sep = 1;
 				i++;
 			}
+			//
+
+			//SEPARATE
 			if (s[i] == SEPARATOR_CHAR)
 				ERROR(MANY_SEPARATORS, the_asm->curr_line_n);
 			if (len == 0 && met_sep == 1)
@@ -82,6 +99,7 @@ void	write_cmnd_line_words(t_asm *the_asm, t_line **line)
 				if (s[i] == SEPARATOR_CHAR)
 					ERROR(MANY_SEPARATORS, the_asm->curr_line_n);
 			}
+			//
 		}
 		the_asm->line_words[w_i] = ft_strsub(s, start, len);
 		w_i++;
